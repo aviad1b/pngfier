fee505303ae4e0c142325c1e8ad6d821fe2de841
aviad1b <aviad.bloch10@gmail.com>
2026-08-11 23:11:49 +0300

    Chore: Added basic CLI infrastructure

--- crates/cli/src/commands.rs
+++ crates/cli/src/commands.rs
@@ -1,4 +1,39 @@
 use anyhow::{Result, bail};
+use clap::Subcommand;
+
+// pngfier compile <input> <img-query>|<img-path> [<key-file>]
+// pngfier extract <image> <output> [<key-file>]
+
+#[derive(Subcommand)]
+pub enum Command {
+    Compile {
+        /// Input file to compile into a PNG.
+        input: String,
+
+        /// Query of image to generate.
+        #[arg(short = 'q', long)]
+        img_query: Option<String>,
+
+        /// Path to image file to base off of.
+        #[arg(short = 'p', long)]
+        img_path: Option<String>,
+
+        /// Optional path to store key to (instead of using PNG riding).
+        #[arg(long)]
+        key_file: Option<String>
+    },
+    Extract {
+        /// Path to compiled image to extract data from.
+        img_path: String,
+
+        /// File path to store extracted data to.
+        output: String,
+
+        /// Optional path to read key from (instead of assuming PNG riding).
+        #[arg(long)]
+        key_file: Option<String>,
+    },
+}
 
 /// Represents image source (search query / direct path).
 pub enum ImgSrc {
--- crates/cli/src/main.rs
+++ crates/cli/src/main.rs
@@ -1,3 +1,41 @@
-fn main() {
-    println!("Hello There");
+use anyhow::Result;
+use clap::Parser;
+
+use crate::commands::{Command, ImgSrc};
+
+mod commands;
+
+/// Main CLI parser.
+#[derive(Parser)]
+struct Cli {
+    #[command(subcommand)]
+    command: Command,
+}
+
+fn main() -> Result<()> {
+    let cli = Cli::parse();
+    match cli.command {
+        Command::Compile { input, img_query, img_path, key_file } =>
+            handle_compile(input, ImgSrc::from_args(img_query, img_path)?, key_file),
+        Command::Extract { img_path, output, key_file } =>
+            handle_extract(img_path, output, key_file),
+    }
+}
+
+/// Handles 'compile' command.
+/// * `input` - Input file to compile into a PNG.
+/// * `img_src` - Image source path (of image to override).
+/// * `key_file` - Optional path to store key to (instead of using PNG riding).
+/// Returns error if occured.
+fn handle_compile(input: String, img_src: ImgSrc, key_file: Option<String>) -> Result<()> {
+    todo!()
+}
+
+/// Handles 'extract' command.
+/// * `img_path` - Path to compiled image to extract data from.
+/// * `output` - File path to store extracted data to.
+/// * `key_file` - Optional path to read key from (instead of assuming PNG riding).
+/// Returns error if occured.
+fn handle_extract(img_path: String, output: String, key_file: Option<String>) -> Result<()> {
+    todo!()
 }

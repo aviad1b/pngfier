f8fe51f03913dbebe7e920817114a368255dd7b0
aviad1b <aviad.bloch10@gmail.com>
2026-08-12 03:38:04 +0300

    Chore: Added ConstBinParsible trait

---
+++ crates/core/src/streams/traits.rs
@@ -0,0 +1,21 @@
+use generic_array::{ArrayLength, GenericArray, typenum::U1};
+
+/// Used for types that can be parsed from&to bytes, using a fixed-sized buffer.
+pub trait ConstBinParsible {
+    /// Size of buffer needed for parsing.
+    type BuffSize: ArrayLength;
+
+    /// Parses `Self` from a corresponding buffer of bytes.
+    /// 
+    /// * `buff` - Buffer to parse `Self` from.
+    /// 
+    /// Returns parsed `Self` instance.
+    /// 
+    fn const_bin_parse(buff: &GenericArray<u8, Self::BuffSize>) -> Self;
+
+    /// Parses `self` into a corresponding buffer of bytes.
+    /// 
+    /// * - `buff` - Buffer to parse into.
+    /// 
+    fn const_bin_unparse(&self, buff: &mut GenericArray<u8, Self::BuffSize>);
+}

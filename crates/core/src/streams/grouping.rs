cc73c9e7ae1498b47e95a37a7c17aac64603d1fe
aviad1b <aviad.bloch10@gmail.com>
2026-08-16 00:59:27 +0300

    Feat: Implemented GroupedElemStreams::new

--- crates/core/src/streams/grouping.rs
+++ crates/core/src/streams/grouping.rs
@@ -39,8 +39,7 @@ impl<'a, E, N: ArrayLength, S: Stream> GroupedElemStreams<'a, E, N, S> {
     /// Returns constructed instance.
     /// 
     pub fn new(streams: GenericArray<&'a mut S, N>) -> Self {
-        let _ = streams;
-        todo!() // TODO: Implement
+        Self { streams, phantom: PhantomData }
     }
 }
 

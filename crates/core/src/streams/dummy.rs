31d3093b16559e8ca3f4c6158b464f8fea91a2e0
aviad1b <aviad.bloch10@gmail.com>
2026-08-16 00:54:47 +0300

    Chore: Added dummy binary stream (mostly for tests, uses underlying Cursor so does not have tests of its own for now)

--- crates/core/src/streams/dummy.rs
+++ crates/core/src/streams/dummy.rs
@@ -1,3 +1,9 @@
+use std::io::{self, Cursor, Read, Write};
+
+use bitstream_io::{BitRead, BitReader, BitWrite, BitWriter, Endianness};
+
+use crate::streams::traits::{InputBinaryStream, OutputBinaryStream};
+
 use super::{
     StreamPos,
     traits::{
@@ -29,27 +35,27 @@ impl<E: Copy> DummyInputElemStream<E> {
 }
 
 impl<E: Copy> Stream for DummyInputElemStream<E> {
-    fn rewind(&mut self) -> std::io::Result<()> {
+    fn rewind(&mut self) -> io::Result<()> {
         self.pos = 0;
         Ok(())
     }
 
-    fn get_pos(&mut self) -> std::io::Result<StreamPos> {
+    fn get_pos(&mut self) -> io::Result<StreamPos> {
         Ok(self.pos)
     }
 
-    fn set_pos(&mut self, pos: StreamPos) -> std::io::Result<()> {
+    fn set_pos(&mut self, pos: StreamPos) -> io::Result<()> {
         self.pos = pos;
         Ok(())
     }
 
-    fn get_size(&mut self) -> std::io::Result<StreamPos> {
+    fn get_size(&mut self) -> io::Result<StreamPos> {
         Ok(self.elems.len() as StreamPos)
     }
 }
 
 impl<E: Copy> InputElemStream<E> for DummyInputElemStream<E> {
-    fn read_next_elem(&mut self) -> std::io::Result<Option<E>> {
+    fn read_next_elem(&mut self) -> io::Result<Option<E>> {
         if self.pos >= self.elems.len() as StreamPos {
             return Ok(None); // end-of-stream
         }
@@ -86,27 +92,27 @@ impl<E: Copy> DummyOutputElemStream<E> {
 }
 
 impl<E: Copy> Stream for DummyOutputElemStream<E> {
-    fn rewind(&mut self) -> std::io::Result<()> {
+    fn rewind(&mut self) -> io::Result<()> {
         self.pos = 0;
         Ok(())
     }
 
-    fn get_pos(&mut self) -> std::io::Result<StreamPos> {
+    fn get_pos(&mut self) -> io::Result<StreamPos> {
         Ok(self.pos)
     }
 
-    fn set_pos(&mut self, pos: StreamPos) -> std::io::Result<()> {
+    fn set_pos(&mut self, pos: StreamPos) -> io::Result<()> {
         self.pos = pos;
         Ok(())
     }
 
-    fn get_size(&mut self) -> std::io::Result<StreamPos> {
+    fn get_size(&mut self) -> io::Result<StreamPos> {
         Ok(self.elems.len() as StreamPos)
     }
 }
 
 impl<E: Copy> OutputElemStream<E> for DummyOutputElemStream<E> {
-    fn write_next_elem(&mut self, elem: E) -> std::io::Result<()> {
+    fn write_next_elem(&mut self, elem: E) -> io::Result<()> {
         if self.pos as usize >= self.elems.len() {
             self.elems.push(elem);
         } else {
@@ -116,11 +122,78 @@ impl<E: Copy> OutputElemStream<E> for DummyOutputElemStream<E> {
         Ok(())
     }
 
-    fn truncate(&mut self, len: StreamPos) -> std::io::Result<()> {
+    fn truncate(&mut self, len: StreamPos) -> io::Result<()> {
         if 0 == len {
             self.elems.clear();
         } else {
             self.elems.resize(len as usize, self.elems[0]);
+        }
+        Ok(())
+    }
+}
+
+/// Dummy implementation of `InputBinaryStream` and `OutputBinaryStream` traits, 
+/// uses runtime vector.
+pub struct DummyBinaryStream {
+    cursor: Cursor<Vec<u8>>,
+}
+
+impl DummyBinaryStream {
+    /// Constructs a new instance.
+    /// 
+    /// * `data` - Initial data (bytes vector) for stream.
+    /// 
+    /// Returns constructed instance.
+    /// 
+    pub fn new(data: Vec<u8>) -> Self {
+        Self { cursor: Cursor::new(data) }
+    }
+}
+
+impl Stream for DummyBinaryStream {
+    fn rewind(&mut self) -> io::Result<()> {
+        self.cursor.set_position(0);
+        Ok(())
+    }
+
+    fn get_pos(&mut self) -> io::Result<StreamPos> {
+        Ok(self.cursor.position() as StreamPos)
+    }
+
+    fn set_pos(&mut self, pos: StreamPos) -> io::Result<()> {
+        self.cursor.set_position(pos as u64);
+        Ok(())
+    }
+
+    fn get_size(&mut self) -> io::Result<StreamPos> {
+        Ok(self.cursor.get_ref().len() as StreamPos)
+    }
+}
+
+impl InputBinaryStream for DummyBinaryStream {
+    fn read_bytes(&mut self, buff: &mut [u8]) -> io::Result<()> {
+        self.cursor.read_exact(buff)
+    }
+
+    fn obtain_bits_reader(&mut self, endianness: impl Endianness) -> io::Result<impl BitRead> {
+        Ok(BitReader::endian(&mut self.cursor, endianness))
+    }
+}
+
+impl OutputBinaryStream for DummyBinaryStream {
+    fn write_bytes(&mut self, buff: &[u8]) -> io::Result<()> {
+        self.cursor.write_all(buff)
+    }
+
+    fn obtain_bits_writer(&mut self, endianness: impl Endianness) -> io::Result<impl BitWrite> {
+        Ok(BitWriter::endian(&mut self.cursor, endianness))
+    }
+
+    fn truncate(&mut self, len: StreamPos) -> io::Result<()> {
+        let pos = self.cursor.position();
+        self.cursor.get_mut().resize(len as usize, 0);
+        if pos > len as u64 {
+            self.cursor.set_position(len as u64);
         }
         Ok(())
     }

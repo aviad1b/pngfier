b3d2918cdf1b3b2554f0a97e5a7b97c1b26ffb86
aviad1b <aviad.bloch10@gmail.com>
2026-08-16 01:28:37 +0300

    Test: Added test grouped elem streams write grows correctly

--- crates/core/src/tests/test_streams/test_grouping.rs
+++ crates/core/src/tests/test_streams/test_grouping.rs
@@ -206,3 +206,19 @@ fn grouped_binary_streams_bits_reader_writer_roundtrip() {
 		assert_eq!(low, 0b0101);
 	}
 }
+
+#[test]
+fn grouped_elem_streams_write_grows_correctly() {
+    let mut s0 = DummyOutputElemStream::new(vec![0x00; 1]);
+    let mut s1 = DummyOutputElemStream::new(vec![0x00; 1]);
+    let arr: GenericArray<&mut DummyOutputElemStream<i32>, U2> =
+        GenericArray::from_iter([&mut s0, &mut s1]);
+    let mut grouped = GroupedElemStreams::new(arr);
+
+    grouped.write_next_elem::<0>(0x01).unwrap(); // overwrite
+    grouped.write_next_elem::<0>(0x02).unwrap(); // grow
+    grouped.write_next_elem::<0>(0x03).unwrap(); // grow
+
+    assert_eq!(s0.get_all(), &[0x01, 0x02, 0x03]);
+    assert_eq!(s1.get_all(), &[0x00]); // untouched
+}

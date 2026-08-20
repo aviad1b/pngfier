0778f59112be38ba59f0f0eae737c8a5a5d87c7a
aviad1b <aviad.bloch10@gmail.com>
2026-08-20 10:16:29 +0300

    Chore: Added declaration of ReachMapper

--- crates/core/src/chunks/mapping/reach.rs
+++ crates/core/src/chunks/mapping/reach.rs
@@ -1,3 +1,7 @@
+use std::io;
+
+use crate::elems::Elem;
+
 use super::super::{ChunkIndex, ChunkSize};
 
 // To temporarily store paths of chunks that exist in both image and data
@@ -17,4 +21,38 @@ pub struct MatchInfo {
 
     /// Where in `image` this match starts
     pub src_start: ChunkIndex,
+}
+
+/// Abstraction of reach mapping.
+/// A mapping between an index in the input data and the furthest index we 
+/// can reach in the data such that the chunk formed between said two indexes 
+/// exists in the input image.
+/// 
+/// * `E` - Element type.
+/// 
+pub trait ReachMapper<E: Elem> {
+    /// Gets amount of indexes mapped.
+    /// 
+    /// NOTE: Returns as `ChunkIndex` for convenience of comparison.
+    /// 
+    fn len(&self) -> ChunkIndex;
+
+    /// Gets best match found starting exactly at data-position `index`.
+    /// 
+    /// * `index` - Data index to get mapping of.
+    /// 
+    /// Returns best match for `index`, as stated above.
+    /// If nothing matches there, returns MatchInfo { reach: index, src_start: index } (no progress).
+    fn get(&self, index: ChunkIndex) -> MatchInfo;
+
+    /// Gets literal elements from input data.
+    /// 
+    /// * `start` - Data index to start at.
+    /// * `count` - Amount of elements to get.
+    /// 
+    /// Returns vector of elements, or error if occurred.
+    /// 
+    /// NOTE: This method it `mut` as it may need to read from an internal file (and move its cursor).
+    /// 
+    fn get_elems(&mut self, start: ChunkIndex, count: ChunkSize) -> io::Result<Vec<E>>;
 }

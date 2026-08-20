1339e3687db320cc691adeb136aad3e90075ccca
aviad1b <aviad.bloch10@gmail.com>
2026-08-20 06:38:02 +0300

    Test: Added test runtime insertion gives correct contains

--- crates/core/src/tests/test_elems.rs
+++ crates/core/src/tests/test_elems.rs
@@ -30,6 +30,31 @@ fn template_insertion_gives_correct_iteration(mut matrix: impl ElemIndexesMatrix
     assert_eq!(slot_to_set(matrix.at(50, 50).unwrap()), HashSet::from_iter(vec![]));
 }
 
+fn template_insertion_gives_correct_contains(mut matrix: impl ElemIndexesMatrix<u8, i32>) {
+    matrix.at_mut(0, 0).unwrap().insert(4).unwrap();
+    matrix.at_mut(0, 0).unwrap().insert(3).unwrap();
+    matrix.at_mut(0, 0).unwrap().insert(5).unwrap();
+
+    matrix.at_mut(4, 3).unwrap().insert(5).unwrap();
+    
+    matrix.at_mut(255, 255).unwrap().insert(1000).unwrap();
+    matrix.at_mut(255, 255).unwrap().insert(2222).unwrap();
+
+    assert!(matrix.at(0, 0).unwrap().contains(&3).unwrap());
+    assert!(matrix.at(0, 0).unwrap().contains(&4).unwrap());
+    assert!(matrix.at(0, 0).unwrap().contains(&5).unwrap());
+    assert!(!matrix.at(0, 0).unwrap().contains(&6).unwrap());
+    
+    assert!(matrix.at(4, 3).unwrap().contains(&5).unwrap());
+    assert!(!matrix.at(4, 3).unwrap().contains(&3).unwrap());
+    
+    assert!(matrix.at(255, 255).unwrap().contains(&1000).unwrap());
+    assert!(matrix.at(255, 255).unwrap().contains(&2222).unwrap());
+    assert!(!matrix.at(255, 255).unwrap().contains(&3333).unwrap());
+    
+    assert!(!matrix.at(50, 50).unwrap().contains(&0).unwrap());
+}
+
 ////////////////////////////////////////////////////////////////
 // ---------------- RuntimeElemIndexesMatrix ---------------- //
 ////////////////////////////////////////////////////////////////
@@ -37,4 +62,9 @@ fn template_insertion_gives_correct_iteration(mut matrix: impl ElemIndexesMatrix
 #[test]
 fn runtime_insertion_gives_correct_iteration() {
    template_insertion_gives_correct_iteration(RuntimeElemIndexesMatrix::new());
+}
+
+#[test]
+fn runtime_insertion_gives_correct_contains() {
+    template_insertion_gives_correct_contains(RuntimeElemIndexesMatrix::new());
 }

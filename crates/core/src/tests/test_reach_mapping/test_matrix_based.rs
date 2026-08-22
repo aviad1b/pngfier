3b930f9e7292d66477eb8ac831c5240938e64fa0
aviad1b <aviad.bloch10@gmail.com>
2026-08-22 08:55:28 +0300

    Test: Added test_get_path_starts_vec

--- crates/core/src/tests/test_reach_mapping/test_matrix_based.rs
+++ crates/core/src/tests/test_reach_mapping/test_matrix_based.rs
@@ -1,6 +1,6 @@
 use std::{collections::HashSet, hash::Hash};
 
-use crate::{elems::{ElemIndexesMatrix, ElemIndexesMatrixSlot, RuntimeElemIndexesMatrix}, streams::dummy::DummyInputElemStream};
+use crate::{chunks::ChunkIndex, elems::{ElemIndexesMatrix, ElemIndexesMatrixSlot, RuntimeElemIndexesMatrix}, streams::dummy::DummyInputElemStream};
 
 use super::super::super::chunks::mapping::{reach::*, reach_utils::*};
 
@@ -10,6 +10,13 @@ fn slot_to_set<I: Eq + Copy + Hash, Slot: ElemIndexesMatrixSlot<I>>(slot: Slot) 
         res.insert(*elem);
     }
     res
+}
+
+fn paths_src_starts<'a, T>(paths: T) -> HashSet<ChunkIndex>
+where
+    T: Iterator<Item = &'a Path>
+{
+    HashSet::from_iter(paths.map(|path| path.src_start))
 }
 
 #[test]
@@ -62,4 +69,58 @@ fn test_init_img_matrix() {
     assert_eq!(slot_to_set(img_matrix.at(b'E', b'F').unwrap()), HashSet::from_iter(vec![6]));
     
     assert_eq!(slot_to_set(img_matrix.at(b'F', b'A').unwrap()), HashSet::from_iter(vec![7]));
+}
+
+#[test]
+fn test_get_path_starts_vec() {
+    let mut image = DummyInputElemStream::new(b"ABCDABEFABCD".to_vec());
+    let mut img_matrix = RuntimeElemIndexesMatrix::<u8, _>::new();
+    init_img_matrix(&mut image, &mut img_matrix).unwrap();
+
+    let mut data = DummyInputElemStream::new(b"ABEFABCDZABCDDA".to_vec());
+
+    let paths = get_path_starts_vec(&mut data, &img_matrix, 0).unwrap();
+    assert_eq!(paths_src_starts(paths.iter()), HashSet::from_iter(vec![0, 4, 8]));
+
+    let paths = get_path_starts_vec(&mut data, &img_matrix, 1).unwrap();
+    assert_eq!(paths_src_starts(paths.iter()), HashSet::from_iter(vec![5]));
+
+    let paths = get_path_starts_vec(&mut data, &img_matrix, 2).unwrap();
+    assert_eq!(paths_src_starts(paths.iter()), HashSet::from_iter(vec![6]));
+
+    let paths = get_path_starts_vec(&mut data, &img_matrix, 3).unwrap();
+    assert_eq!(paths_src_starts(paths.iter()), HashSet::from_iter(vec![7]));
+
+    let paths = get_path_starts_vec(&mut data, &img_matrix, 4).unwrap();
+    assert_eq!(paths_src_starts(paths.iter()), HashSet::from_iter(vec![0, 4, 8]));
+
+    let paths = get_path_starts_vec(&mut data, &img_matrix, 5).unwrap();
+    assert_eq!(paths_src_starts(paths.iter()), HashSet::from_iter(vec![1, 9]));
+
+    let paths = get_path_starts_vec(&mut data, &img_matrix, 6).unwrap();
+    assert_eq!(paths_src_starts(paths.iter()), HashSet::from_iter(vec![2, 10]));
+
+    let paths = get_path_starts_vec(&mut data, &img_matrix, 7).unwrap();
+    assert_eq!(paths_src_starts(paths.iter()), HashSet::from_iter(vec![]));
+
+    let paths = get_path_starts_vec(&mut data, &img_matrix, 8).unwrap();
+    assert_eq!(paths_src_starts(paths.iter()), HashSet::from_iter(vec![]));
+
+    let paths = get_path_starts_vec(&mut data, &img_matrix, 9).unwrap();
+    assert_eq!(paths_src_starts(paths.iter()), HashSet::from_iter(vec![0, 4, 8]));
+
+    let paths = get_path_starts_vec(&mut data, &img_matrix, 10).unwrap();
+    assert_eq!(paths_src_starts(paths.iter()), HashSet::from_iter(vec![1, 9]));
+
+    let paths = get_path_starts_vec(&mut data, &img_matrix, 11).unwrap();
+    assert_eq!(paths_src_starts(paths.iter()), HashSet::from_iter(vec![2, 10]));
+
+    let paths = get_path_starts_vec(&mut data, &img_matrix, 12).unwrap();
+    assert_eq!(paths_src_starts(paths.iter()), HashSet::from_iter(vec![]));
+
+    let paths = get_path_starts_vec(&mut data, &img_matrix, 13).unwrap();
+    assert_eq!(paths_src_starts(paths.iter()), HashSet::from_iter(vec![3]));
+
+    let paths = get_path_starts_vec(&mut data, &img_matrix, 14).unwrap();
+    assert_eq!(paths_src_starts(paths.iter()), HashSet::from_iter(vec![]));
 }

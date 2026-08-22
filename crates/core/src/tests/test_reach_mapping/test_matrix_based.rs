a518ff4b70b905ae83ab1d22fd9b6a31c7986ecf
aviad1b <aviad.bloch10@gmail.com>
2026-08-22 08:22:02 +0300

    Test: Modified test_init_img_matrix for clearer asserts

--- crates/core/src/tests/test_reach_mapping/test_matrix_based.rs
+++ crates/core/src/tests/test_reach_mapping/test_matrix_based.rs
@@ -1,6 +1,16 @@
+use std::{collections::HashSet, hash::Hash};
+
 use crate::{elems::{ElemIndexesMatrix, ElemIndexesMatrixSlot, RuntimeElemIndexesMatrix}, streams::dummy::DummyInputElemStream};
 
 use super::super::super::chunks::mapping::{reach::*, reach_utils::*};
+
+fn slot_to_set<I: Eq + Copy + Hash, Slot: ElemIndexesMatrixSlot<I>>(slot: Slot) -> HashSet<I> {
+    let mut res = HashSet::new();
+    for elem in slot.iter().unwrap() {
+        res.insert(*elem);
+    }
+    res
+}
 
 #[test]
 fn test_init_img_matrix() {
@@ -9,58 +19,47 @@ fn test_init_img_matrix() {
 
     init_img_matrix(&mut image, &mut img_matrix).unwrap();
 
-    assert!(Vec::from_iter(img_matrix.at(b'A', b'A').unwrap().iter().unwrap()).is_empty());
-    assert!(Vec::from_iter(img_matrix.at(b'A', b'C').unwrap().iter().unwrap()).is_empty());
-    assert!(Vec::from_iter(img_matrix.at(b'A', b'D').unwrap().iter().unwrap()).is_empty());
-    assert!(Vec::from_iter(img_matrix.at(b'A', b'E').unwrap().iter().unwrap()).is_empty());
-    assert!(Vec::from_iter(img_matrix.at(b'A', b'F').unwrap().iter().unwrap()).is_empty());
-    assert!(Vec::from_iter(img_matrix.at(b'B', b'A').unwrap().iter().unwrap()).is_empty());
-    assert!(Vec::from_iter(img_matrix.at(b'B', b'B').unwrap().iter().unwrap()).is_empty());
-    assert!(Vec::from_iter(img_matrix.at(b'B', b'D').unwrap().iter().unwrap()).is_empty());
-    assert!(Vec::from_iter(img_matrix.at(b'B', b'F').unwrap().iter().unwrap()).is_empty());
-    assert!(Vec::from_iter(img_matrix.at(b'C', b'A').unwrap().iter().unwrap()).is_empty());
-    assert!(Vec::from_iter(img_matrix.at(b'C', b'B').unwrap().iter().unwrap()).is_empty());
-    assert!(Vec::from_iter(img_matrix.at(b'C', b'C').unwrap().iter().unwrap()).is_empty());
-    assert!(Vec::from_iter(img_matrix.at(b'C', b'E').unwrap().iter().unwrap()).is_empty());
-    assert!(Vec::from_iter(img_matrix.at(b'C', b'F').unwrap().iter().unwrap()).is_empty());
-    assert!(Vec::from_iter(img_matrix.at(b'D', b'B').unwrap().iter().unwrap()).is_empty());
-    assert!(Vec::from_iter(img_matrix.at(b'D', b'C').unwrap().iter().unwrap()).is_empty());
-    assert!(Vec::from_iter(img_matrix.at(b'D', b'D').unwrap().iter().unwrap()).is_empty());
-    assert!(Vec::from_iter(img_matrix.at(b'D', b'E').unwrap().iter().unwrap()).is_empty());
-    assert!(Vec::from_iter(img_matrix.at(b'D', b'F').unwrap().iter().unwrap()).is_empty());
-    assert!(Vec::from_iter(img_matrix.at(b'E', b'A').unwrap().iter().unwrap()).is_empty());
-    assert!(Vec::from_iter(img_matrix.at(b'E', b'B').unwrap().iter().unwrap()).is_empty());
-    assert!(Vec::from_iter(img_matrix.at(b'E', b'C').unwrap().iter().unwrap()).is_empty());
-    assert!(Vec::from_iter(img_matrix.at(b'E', b'D').unwrap().iter().unwrap()).is_empty());
-    assert!(Vec::from_iter(img_matrix.at(b'E', b'E').unwrap().iter().unwrap()).is_empty());
-    assert!(Vec::from_iter(img_matrix.at(b'F', b'B').unwrap().iter().unwrap()).is_empty());
-    assert!(Vec::from_iter(img_matrix.at(b'F', b'C').unwrap().iter().unwrap()).is_empty());
-    assert!(Vec::from_iter(img_matrix.at(b'F', b'D').unwrap().iter().unwrap()).is_empty());
-    assert!(Vec::from_iter(img_matrix.at(b'F', b'E').unwrap().iter().unwrap()).is_empty());
-    assert!(Vec::from_iter(img_matrix.at(b'F', b'F').unwrap().iter().unwrap()).is_empty());
+    assert_eq!(slot_to_set(img_matrix.at(b'A', b'A').unwrap()), HashSet::new());
+    assert_eq!(slot_to_set(img_matrix.at(b'A', b'C').unwrap()), HashSet::new());
+    assert_eq!(slot_to_set(img_matrix.at(b'A', b'D').unwrap()), HashSet::new());
+    assert_eq!(slot_to_set(img_matrix.at(b'A', b'E').unwrap()), HashSet::new());
+    assert_eq!(slot_to_set(img_matrix.at(b'A', b'F').unwrap()), HashSet::new());
+    assert_eq!(slot_to_set(img_matrix.at(b'B', b'A').unwrap()), HashSet::new());
+    assert_eq!(slot_to_set(img_matrix.at(b'B', b'B').unwrap()), HashSet::new());
+    assert_eq!(slot_to_set(img_matrix.at(b'B', b'D').unwrap()), HashSet::new());
+    assert_eq!(slot_to_set(img_matrix.at(b'B', b'F').unwrap()), HashSet::new());
+    assert_eq!(slot_to_set(img_matrix.at(b'C', b'A').unwrap()), HashSet::new());
+    assert_eq!(slot_to_set(img_matrix.at(b'C', b'B').unwrap()), HashSet::new());
+    assert_eq!(slot_to_set(img_matrix.at(b'C', b'C').unwrap()), HashSet::new());
+    assert_eq!(slot_to_set(img_matrix.at(b'C', b'E').unwrap()), HashSet::new());
+    assert_eq!(slot_to_set(img_matrix.at(b'C', b'F').unwrap()), HashSet::new());
+    assert_eq!(slot_to_set(img_matrix.at(b'D', b'B').unwrap()), HashSet::new());
+    assert_eq!(slot_to_set(img_matrix.at(b'D', b'C').unwrap()), HashSet::new());
+    assert_eq!(slot_to_set(img_matrix.at(b'D', b'D').unwrap()), HashSet::new());
+    assert_eq!(slot_to_set(img_matrix.at(b'D', b'E').unwrap()), HashSet::new());
+    assert_eq!(slot_to_set(img_matrix.at(b'D', b'F').unwrap()), HashSet::new());
+    assert_eq!(slot_to_set(img_matrix.at(b'E', b'A').unwrap()), HashSet::new());
+    assert_eq!(slot_to_set(img_matrix.at(b'E', b'B').unwrap()), HashSet::new());
+    assert_eq!(slot_to_set(img_matrix.at(b'E', b'C').unwrap()), HashSet::new());
+    assert_eq!(slot_to_set(img_matrix.at(b'E', b'D').unwrap()), HashSet::new());
+    assert_eq!(slot_to_set(img_matrix.at(b'E', b'E').unwrap()), HashSet::new());
+    assert_eq!(slot_to_set(img_matrix.at(b'F', b'B').unwrap()), HashSet::new());
+    assert_eq!(slot_to_set(img_matrix.at(b'F', b'C').unwrap()), HashSet::new());
+    assert_eq!(slot_to_set(img_matrix.at(b'F', b'D').unwrap()), HashSet::new());
+    assert_eq!(slot_to_set(img_matrix.at(b'F', b'E').unwrap()), HashSet::new());
+    assert_eq!(slot_to_set(img_matrix.at(b'F', b'F').unwrap()), HashSet::new());
     
-    assert_eq!(Vec::from_iter(img_matrix.at(b'A', b'B').unwrap().iter().unwrap()).len(), 3);
-    assert!(img_matrix.at(b'A', b'B').unwrap().contains(&1).unwrap());
-    assert!(img_matrix.at(b'A', b'B').unwrap().contains(&5).unwrap());
-    assert!(img_matrix.at(b'A', b'B').unwrap().contains(&9).unwrap());
+    assert_eq!(slot_to_set(img_matrix.at(b'A', b'B').unwrap()), HashSet::from_iter(vec![1, 5, 9]));
     
-    assert_eq!(Vec::from_iter(img_matrix.at(b'B', b'C').unwrap().iter().unwrap()).len(), 2);
-    assert!(img_matrix.at(b'B', b'C').unwrap().contains(&2).unwrap());
-    assert!(img_matrix.at(b'B', b'C').unwrap().contains(&10).unwrap());
+    assert_eq!(slot_to_set(img_matrix.at(b'B', b'C').unwrap()), HashSet::from_iter(vec![2, 10]));
     
-    assert_eq!(Vec::from_iter(img_matrix.at(b'B', b'E').unwrap().iter().unwrap()).len(), 1);
-    assert!(img_matrix.at(b'B', b'E').unwrap().contains(&6).unwrap());
+    assert_eq!(slot_to_set(img_matrix.at(b'B', b'E').unwrap()), HashSet::from_iter(vec![6]));
     
-    assert_eq!(Vec::from_iter(img_matrix.at(b'C', b'D').unwrap().iter().unwrap()).len(), 2);
-    assert!(img_matrix.at(b'C', b'D').unwrap().contains(&3).unwrap());
-    assert!(img_matrix.at(b'C', b'D').unwrap().contains(&11).unwrap());
+    assert_eq!(slot_to_set(img_matrix.at(b'C', b'D').unwrap()), HashSet::from_iter(vec![3, 11]));
     
-    assert_eq!(Vec::from_iter(img_matrix.at(b'D', b'A').unwrap().iter().unwrap()).len(), 1);
-    assert!(img_matrix.at(b'D', b'A').unwrap().contains(&4).unwrap());
+    assert_eq!(slot_to_set(img_matrix.at(b'D', b'A').unwrap()), HashSet::from_iter(vec![4]));
     
-    assert_eq!(Vec::from_iter(img_matrix.at(b'E', b'F').unwrap().iter().unwrap()).len(), 1);
-    assert!(img_matrix.at(b'E', b'F').unwrap().contains(&7).unwrap());
+    assert_eq!(slot_to_set(img_matrix.at(b'E', b'F').unwrap()), HashSet::from_iter(vec![7]));
     
-    assert_eq!(Vec::from_iter(img_matrix.at(b'F', b'A').unwrap().iter().unwrap()).len(), 1);
-    assert!(img_matrix.at(b'F', b'A').unwrap().contains(&8).unwrap());
+    assert_eq!(slot_to_set(img_matrix.at(b'F', b'A').unwrap()), HashSet::from_iter(vec![8]));
 }

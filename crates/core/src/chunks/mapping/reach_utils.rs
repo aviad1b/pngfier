94759d795dcff447dd121f51febfdb4e35894f63
aviad1b <aviad.bloch10@gmail.com>
2026-08-22 21:49:50 +0300

    Chore: Made Path derive Debug

--- crates/core/src/chunks/mapping/reach_utils.rs
+++ crates/core/src/chunks/mapping/reach_utils.rs
@@ -9,7 +9,7 @@ use super::super::{ChunkIndex, ChunkSize};
 
 // To temporarily store paths of chunks that exist in both image and data
 // `src_start` is starting index at image.
-#[derive(Clone, Copy, PartialEq, Eq)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub struct Path {
     pub len: ChunkSize,
     pub src_start: ChunkIndex,

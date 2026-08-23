0c7c31b898719b685bbfdff2804cfec3ed825479
aviad1b <aviad.bloch10@gmail.com>
2026-08-23 23:52:13 +0300

    Chore: Replaced TempFile with OutputFile

--- crates/cli/src/utils.rs
+++ crates/cli/src/utils.rs
@@ -1,26 +1,22 @@
 use std::sync::atomic::{AtomicU64, Ordering};
 
-/// Temporary file with automatically generated unique path.
-/// File gets deleted automatically at end of scope.
-pub struct TempFile {
+/// Output file with automatically generated unique path.
+pub struct OutputFile {
     path: std::path::PathBuf,
 }
 
-impl TempFile {
-    /// Constructs a new `TempFile` instance.
-    /// 
-    /// * `name` - Base name to base file path off.
+impl OutputFile {
+    /// Constructs a new `OutputFile` instance.
     /// 
     /// Returns constructed instance.
     /// 
-    pub fn new(name: &str) -> Self {
+    pub fn new() -> Self {
         static COUNTER: AtomicU64 = AtomicU64::new(0);
         let id = COUNTER.fetch_add(1, Ordering::SeqCst);
         let mut path = std::env::temp_dir();
         path.push(format!(
-            "pngfier_{}_{}_{}",
+            "pngfier_output_{}_{}",
             std::process::id(),
-            name,
             id
         ));
         Self { path }
@@ -29,12 +25,5 @@ impl TempFile {
     /// Gets file's path as a string slice.
     pub fn path_str(&self) -> &str {
         self.path.to_str().unwrap()
-    }
-}
-
-impl Drop for TempFile {
-    fn drop(&mut self) {
-        // delete temp file on scope exit
-        let _ = std::fs::remove_file(&self.path);
     }
 }

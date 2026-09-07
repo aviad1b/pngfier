ffd03ccb8e804f197b6ca386bb843e874cb9da4e
aviad1b <aviad.bloch10@gmail.com>
2026-09-08 01:27:47 +0300

    Style: Removed excessive borrows

--- crates/cli/src/configs/compile.rs
+++ crates/cli/src/configs/compile.rs
@@ -67,7 +67,7 @@ pub fn apply<E: Elem>(widths: &ChunkInfoWidths,
         (ImgSrc::Path(_), None) => bail!("Key file is mandatory for now."),
         (ImgSrc::Path(in_img_path), Some(out_key_path)) => path_with_key(
             |streams| callbacks::compile::<E, _, _>(widths, streams),
-            &in_img_path, &in_file, &out_img, &out_key_path
+            in_img_path, in_file, out_img, out_key_path
         ),
     }
 }

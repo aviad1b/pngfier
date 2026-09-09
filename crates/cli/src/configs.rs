5b7dfaec40087236165ee11c9ce47e50e5e030c5
aviad1b <aviad.bloch10@gmail.com>
2026-09-09 05:54:11 +0300

    Refactor: Made apply_compile return space overhead

--- crates/cli/src/configs.rs
+++ crates/cli/src/configs.rs
@@ -14,11 +14,11 @@ mod extract;
 /// * `img_src` - Image source path (of image to override).
 /// * `key_file` - Optional path to store key to (instead of using PNG riding).
 /// 
-/// Returns error if occurred.
+/// Returns space overhead (fraction), or error if occurred.
 /// 
 pub fn apply_compile<E: Elem>(widths: &ChunkInfoWidths,
                               out_img: &String, in_file: &String,
-                              img_src: &ImgSrc, key_file: &Option<String>) -> Result<()> {
+                              img_src: &ImgSrc, key_file: &Option<String>) -> Result<f64> {
     match (img_src, key_file) {
         (ImgSrc::Query(_), None) => bail!("Query-based compiling is not supported yet."),
         (ImgSrc::Query(_), Some(_)) => bail!("Query-based compiling is not supported yet."),
@@ -27,8 +27,7 @@ pub fn apply_compile<E: Elem>(widths: &ChunkInfoWidths,
             |streams| callbacks::compile::<E, _, _>(widths, streams),
             in_img_path, in_file, out_img, out_key_path
         ),
-    }?;
-    Ok(())
+    }
 }
 
 /// Runs extract operation (selecting configuration based on params).
